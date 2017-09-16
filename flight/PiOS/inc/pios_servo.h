290c8a25a41e619fda13e4de7fef82cacc660eb3
Mario Goebbels <me@tomservo.cc>
2017-09-16 16:52:57 +0200

    pios_servo: Support requesting telemetry from ESC.

--- flight/PiOS/inc/pios_servo.h
+++ flight/PiOS/inc/pios_servo.h
@@ -71,6 +71,8 @@ extern void PIOS_Servo_PrepareForReset();
 extern void PIOS_Servo_Set(uint8_t servo, float position);
 extern void PIOS_Servo_Update(void);
 
+extern int PIOS_Servo_RequestTelemetry(uint8_t servo);
+
 #endif /* PIOS_SERVO_H */
 
 /**
